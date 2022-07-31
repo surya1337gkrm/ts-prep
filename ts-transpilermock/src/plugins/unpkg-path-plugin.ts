af553494465c0169adc9c97e56759d1eaacccca4
surya1337gkrm <suryavenkatesh0@gmail.com>
2022-07-31 06:54:37 -0400

    Basic Build

---
+++ ts-transpilermock/src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,30 @@
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      //handle index.js file
+      build.onResolve({ filter: /(^index\.js$)/ }, (args: any) => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      //handle relative path files in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      //handling main files
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
+};
